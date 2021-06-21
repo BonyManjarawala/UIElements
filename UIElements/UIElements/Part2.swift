e4f6ecc4b03ecf1b49138bb577e214f2ac3f29d6
BonyManjrawala <85922433+BonyManjarawala@users.noreply.github.com>
2021-06-21 19:04:59 +0530

    Add files via upload

---
+++ UIElements/UIElements/Part2.swift
@@ -0,0 +1,118 @@
+//
+//  Part2.swift
+//  UIElements
+//
+//  Created by DCS on 20/06/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class Part2: ViewController{
+    
+    private let toolBar:UIToolbar = {
+        let toolBar = UIToolbar()
+        let cancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let gallery = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(handleGallery))
+        let camera = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(handleCamera))
+        toolBar.items = [cancel,flexSpace, gallery, camera]
+        return toolBar
+    }()
+    
+    @objc private func handleCancel() {
+        print("Cancel Called")
+        self.dismiss(animated: true)
+    }
+    
+    @objc private func handleGallery() {
+        print("Gallery Called")
+        imagePicker.sourceType = .photoLibrary
+        DispatchQueue.main.async {
+            self.present(self.imagePicker, animated: true)
+        }
+    }
+    
+    @objc private func handleCamera() {
+        print("camera")
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            DispatchQueue.main.async {
+                self.present(self.imagePicker, animated: true)
+            }
+        } else {
+            let alert = UIAlertController(title: "Sorry!", message: "Camera not found. Try picking an image from your gallery.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    private let tabBar:UITabBar = {
+        let tabBar = UITabBar()
+        let history = UITabBarItem(tabBarSystemItem: .history, tag: 1)
+        let downloads = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+        tabBar.items = [history, downloads]
+        return tabBar
+    }()
+    
+    
+    private let myImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "image")
+        return imageView
+    }()
+    
+    private let imagePicker:UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        return imagePicker
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Part 3"
+        view.backgroundColor = .white
+        
+        view.addSubview(toolBar)
+        view.addSubview(tabBar)
+        tabBar.delegate = self
+        
+        view.addSubview(myImageView)
+        imagePicker.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let toolBarHeight:CGFloat = view.safeAreaInsets.top + 40
+    toolBar.frame = CGRect(x: 0, y: 0, width: view.width, height: toolBarHeight)
+        
+        let tabBarHeight:CGFloat = view.safeAreaInsets.bottom + 49
+        tabBar.frame = CGRect(x: 0, y: view.height - tabBarHeight, width: view.width, height: tabBarHeight)
+        
+        myImageView.frame = CGRect(x: 20, y: toolBar.bottom + 40, width: view.width - 40, height: 200)
+    }
+}
+
+extension Part2: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item.tag)
+    }
+}
+
+extension Part2:UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[.originalImage] as? UIImage {
+            myImageView.image = selectedImage
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }}
